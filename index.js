b94039672eb83d5c469f3aa25389ec4fb43e0cf2
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-23 18:57:43 +0700

    multer

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const parser = require('body-parser').urlencoded({ extended: false });
 const uid = require('uid');
 const reload = require('reload');
 const multer = require('multer');
+const fs = require('fs');
 
-const upload = multer({ dest: './public' });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, uid() + file.originalname)
+});
+
+const upload = multer({ storage });
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -24,21 +31,31 @@ app.get('/add', (req, res) => {
     res.render('add');
 });
 
-app.post('/singer', parser, (req, res) => {
-   const { name, image } = req.body;
+app.get('/remove/:id', (req, res) => {
+    const { id } = req.params;
+    const index = singers.findIndex(singer => singer.id === id);
+    fs.unlinkSync('./public/' + singers[index].image);
+    singers.splice(index, 1);
+    res.redirect('/singer');
+});
+
+app.post('/singer', upload.single('image'), (req, res) => {
+   const { name } = req.body;
+   const image = req.file.filename;
    singers.push({ id: uid(), name, image });
    res.redirect('/singer');
 });
 
-app.get('/demoupload', (req, res) => {
-    res.render('demoupload');
-});
+/*
+    app.get('/demoupload', (req, res) => {
+        res.render('demoupload');
+    });
 
-app.post('/demoupload', upload.single('singerImage'), (req, res) => {
-    // res.render('demoupload');
-    res.send('Thanh cong');
-});
-
+    app.post('/demoupload', upload.single('singerImage'), (req, res) => {
+        // res.render('demoupload');
+        res.send('Thanh cong');
+    });
+*/
 app.listen(3000, () => console.log('Server started'));
 
 reload(app);
