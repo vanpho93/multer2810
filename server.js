ae30a32616f22dd39c55f5104c2135dfeab063eb
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-25 18:52:02 +0700

    middleware

--- server.js
+++ server.js
@@ -2,5 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+// Mot cai handler function trung gian
+// 1. Dap tra luon cai request
+// 2. Them thuoc tinh vao req hoac res roi cho di qua
+// 3. console.log
+
+
+// La 1 function (req, res, next) => 
+const myMiddleware = (req, res, next) => {
+    req.user = { name: 'abcd' };
+    console.log('Co nguoi truy cap luc: ' + new Date());
+    // return res.send('aaaaa');
+    next();
+};
+
+app.use(myMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Hello ' + req.user.name);
+});
+
+app.get('/a', (req, res) => {
+    res.send('a');
+});
+
+app.get('/b', (req, res) => {
+    res.send('b');
+});
+
 app.listen(3000, () => console.log('Server started!'));
